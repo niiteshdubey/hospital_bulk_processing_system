a75fa982844eba3c76d179f393e91283286d6f2b
Nitesh Dubey <niteshdubey75@gmail.com>
2026-01-18 12:56:54 +0530

    Added Constants

---
+++ app/constants.py
@@ -0,0 +1,6 @@
+# Hospital Directory API
+BASE_URL = "https://hospital-directory.onrender.com"
+
+APP_TITLE = "Hospital Bulk Processing API"
+
+APP_DESCRIPTION = "Processes hospital CSV uploads to the Hospital Directory API."
